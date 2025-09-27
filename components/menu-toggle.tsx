48d620f5ce1b49582ef9b9bc92ed729dd8487b5f
Steffi Marie <steffilaplagiste@users.noreply.github.com>
2025-09-27 23:57:03 +0200

    Update menu-toggle.tsx

--- components/menu-toggle.tsx
+++ components/menu-toggle.tsx
@@ -5,5 +5,11 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 
