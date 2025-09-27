256f53f548e47016d908a6fdfbc6376b6bd7fed5
Steffi Marie <steffilaplagiste@users.noreply.github.com>
2025-09-27 15:14:50 +0200

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
         default:
           "bg-primary text-primary-foreground shadow-sm hover:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-xs hover:bg-destructive/90",
+         "border border-orange-500 bg-white text-orange-600 hover:bg-orange-500 hover:text-white",
         outline:
           "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground",
         secondary:
